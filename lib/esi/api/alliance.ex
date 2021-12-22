3b7fbef0e08ac6143ae3c119a4732afed7c00afb
Ajaxify <essennar2020@gmail.com>
2021-12-22 15:27:07 -0800

    update to latest ESI v1.10.1

--- lib/esi/api/alliance.ex
+++ lib/esi/api/alliance.ex
@@ -1,116 +1,6 @@
 defmodule ESI.API.Alliance do
 
   @typedoc """
-  Options for [`Alliance.contacts/2`](#contacts/2).
-
-  - `:page` (DEFAULT: `1`) -- Which page of results to return
-  - `:token` -- Access token to use if unable to set a header
-  """
-  @type contacts_opts :: [contacts_opt]
-  @type contacts_opt :: {:page, nil | integer} | {:token, nil | String.t}
-
-
-  @doc """
-  Return contacts of an alliance.
-
-  ## Response Example
-
-  A list of contacts:
-
-      [
-        %{
-          "contact_id" => 2112625428,
-          "contact_type" => "character",
-          "standing" => 9.9
-        }
-      ]
-
-  ## Swagger Source
-
-  This function was generated from the following Swagger operation:
-
-  - `operationId` -- `get_alliances_alliance_id_contacts`
-  - `path` -- `/alliances/{alliance_id}/contacts/`
-
-  [View on ESI Site](https://esi.tech.ccp.is/latest/#!/Contacts/get_alliances_alliance_id_contacts)
-
-  """
-  @spec contacts(alliance_id :: integer, opts :: contacts_opts) :: ESI.Request.t
-  def contacts(alliance_id, opts \\ []) do
-    %ESI.Request{
-      verb: :get,
-      path: "/alliances/#{alliance_id}/contacts/",
-      opts_schema: %{datasource: {:query, :optional}, page: {:query, :optional}, token: {:query, :optional}},
-      opts: Map.new(opts),
-    }
-  end
-
-  @doc """
-  List all active player alliances.
-
-  ## Response Example
-
-  List of Alliance IDs:
-
-      [99000001, 99000002]
-
-  ## Swagger Source
-
-  This function was generated from the following Swagger operation:
-
-  - `operationId` -- `get_alliances`
-  - `path` -- `/alliances/`
-
-  [View on ESI Site](https://esi.tech.ccp.is/latest/#!/Alliance/get_alliances)
-
-  """
-  @spec alliances() :: ESI.Request.t
-  def alliances() do
-    %ESI.Request{
-      verb: :get,
-      path: "/alliances/",
-      opts_schema: %{datasource: {:query, :optional}},
-
-    }
-  end
-
-  @doc """
-  Public information about an alliance.
-
-  ## Response Example
-
-  Public data about an alliance:
-
-      %{
-        "creator_corporation_id" => 45678,
-        "creator_id" => 12345,
-        "date_founded" => "2016-06-26T21:00:00Z",
-        "executor_corporation_id" => 98356193,
-        "name" => "C C P Alliance",
-        "ticker" => "<C C P>"
-      }
-
-  ## Swagger Source
-
-  This function was generated from the following Swagger operation:
-
-  - `operationId` -- `get_alliances_alliance_id`
-  - `path` -- `/alliances/{alliance_id}/`
-
-  [View on ESI Site](https://esi.tech.ccp.is/latest/#!/Alliance/get_alliances_alliance_id)
-
-  """
-  @spec alliance(alliance_id :: integer) :: ESI.Request.t
-  def alliance(alliance_id) do
-    %ESI.Request{
-      verb: :get,
-      path: "/alliances/#{alliance_id}/",
-      opts_schema: %{datasource: {:query, :optional}},
-
-    }
-  end
-
-  @typedoc """
   Options for [`Alliance.contact_labels/2`](#contact_labels/2).
 
   - `:token` -- Access token to use if unable to set a header
@@ -156,8 +46,8 @@ defmodule ESI.API.Alliance do
   Icon URLs for the given alliance id and server:
 
       %{
-        "px128x128" => "https://imageserver.eveonline.com/Alliance/503818424_128.png",
-        "px64x64" => "https://imageserver.eveonline.com/Alliance/503818424_64.png"
+        "px128x128" => "https://images.evetech.net/Alliance/503818424_128.png",
+        "px64x64" => "https://images.evetech.net/Alliance/503818424_64.png"
       }
 
   ## Swagger Source
@@ -177,6 +67,80 @@ defmodule ESI.API.Alliance do
       path: "/alliances/#{alliance_id}/icons/",
       opts_schema: %{datasource: {:query, :optional}},
 
+    }
+  end
+
+  @doc """
+  List all active player alliances.
+
+  ## Response Example
+
+  List of Alliance IDs:
+
+      [99000001, 99000002]
+
+  ## Swagger Source
+
+  This function was generated from the following Swagger operation:
+
+  - `operationId` -- `get_alliances`
+  - `path` -- `/alliances/`
+
+  [View on ESI Site](https://esi.tech.ccp.is/latest/#!/Alliance/get_alliances)
+
+  """
+  @spec alliances() :: ESI.Request.t
+  def alliances() do
+    %ESI.Request{
+      verb: :get,
+      path: "/alliances/",
+      opts_schema: %{datasource: {:query, :optional}},
+
+    }
+  end
+
+  @typedoc """
+  Options for [`Alliance.contacts/2`](#contacts/2).
+
+  - `:page` (DEFAULT: `1`) -- Which page of results to return
+  - `:token` -- Access token to use if unable to set a header
+  """
+  @type contacts_opts :: [contacts_opt]
+  @type contacts_opt :: {:page, nil | integer} | {:token, nil | String.t}
+
+
+  @doc """
+  Return contacts of an alliance.
+
+  ## Response Example
+
+  A list of contacts:
+
+      [
+        %{
+          "contact_id" => 2112625428,
+          "contact_type" => "character",
+          "standing" => 9.9
+        }
+      ]
+
+  ## Swagger Source
+
+  This function was generated from the following Swagger operation:
+
+  - `operationId` -- `get_alliances_alliance_id_contacts`
+  - `path` -- `/alliances/{alliance_id}/contacts/`
+
+  [View on ESI Site](https://esi.tech.ccp.is/latest/#!/Contacts/get_alliances_alliance_id_contacts)
+
+  """
+  @spec contacts(alliance_id :: integer, opts :: contacts_opts) :: ESI.Request.t
+  def contacts(alliance_id, opts \\ []) do
+    %ESI.Request{
+      verb: :get,
+      path: "/alliances/#{alliance_id}/contacts/",
+      opts_schema: %{datasource: {:query, :optional}, page: {:query, :optional}, token: {:query, :optional}},
+      opts: Map.new(opts),
     }
   end
 
@@ -204,6 +168,42 @@ defmodule ESI.API.Alliance do
     %ESI.Request{
       verb: :get,
       path: "/alliances/#{alliance_id}/corporations/",
+      opts_schema: %{datasource: {:query, :optional}},
+
+    }
+  end
+
+  @doc """
+  Public information about an alliance.
+
+  ## Response Example
+
+  Public data about an alliance:
+
+      %{
+        "creator_corporation_id" => 45678,
+        "creator_id" => 12345,
+        "date_founded" => "2016-06-26T21:00:00Z",
+        "executor_corporation_id" => 98356193,
+        "name" => "C C P Alliance",
+        "ticker" => "<C C P>"
+      }
+
+  ## Swagger Source
+
+  This function was generated from the following Swagger operation:
+
+  - `operationId` -- `get_alliances_alliance_id`
+  - `path` -- `/alliances/{alliance_id}/`
+
+  [View on ESI Site](https://esi.tech.ccp.is/latest/#!/Alliance/get_alliances_alliance_id)
+
+  """
+  @spec alliance(alliance_id :: integer) :: ESI.Request.t
+  def alliance(alliance_id) do
+    %ESI.Request{
+      verb: :get,
+      path: "/alliances/#{alliance_id}/",
       opts_schema: %{datasource: {:query, :optional}},
 
     }
--- lib/esi/api/dogma.ex
+++ lib/esi/api/dogma.ex
@@ -1,74 +1,6 @@
 defmodule ESI.API.Dogma do
 
   @doc """
-  Get information on a dogma attribute.
-
-  ## Response Example
-
-  Information about a dogma attribute:
-
-      %{
-        "attribute_id" => 20,
-        "default_value" => 1,
-        "description" => "Factor by which topspeed increases.",
-        "display_name" => "Maximum Velocity Bonus",
-        "high_is_good" => true,
-        "icon_id" => 1389,
-        "name" => "speedFactor",
-        "published" => true,
-        "unit_id" => 124
-      }
-
-  ## Swagger Source
-
-  This function was generated from the following Swagger operation:
-
-  - `operationId` -- `get_dogma_attributes_attribute_id`
-  - `path` -- `/dogma/attributes/{attribute_id}/`
-
-  [View on ESI Site](https://esi.tech.ccp.is/latest/#!/Dogma/get_dogma_attributes_attribute_id)
-
-  """
-  @spec attribute(attribute_id :: integer) :: ESI.Request.t
-  def attribute(attribute_id) do
-    %ESI.Request{
-      verb: :get,
-      path: "/dogma/attributes/#{attribute_id}/",
-      opts_schema: %{datasource: {:query, :optional}},
-
-    }
-  end
-
-  @doc """
-  Get a list of dogma attribute ids.
-
-  ## Response Example
-
-  A list of dogma attribute ids:
-
-      [1, 2, 3]
-
-  ## Swagger Source
-
-  This function was generated from the following Swagger operation:
-
-  - `operationId` -- `get_dogma_attributes`
-  - `path` -- `/dogma/attributes/`
-
-  [View on ESI Site](https://esi.tech.ccp.is/latest/#!/Dogma/get_dogma_attributes)
-
-  """
-  @spec attributes() :: ESI.Request.t
-  def attributes() do
-    %ESI.Request{
-      verb: :get,
-      path: "/dogma/attributes/",
-      opts_schema: %{datasource: {:query, :optional}},
-
-    }
-  end
-
-  @doc """
   Get information on a dogma effect.
 
   ## Response Example
@@ -108,6 +40,45 @@ defmodule ESI.API.Dogma do
   end
 
   @doc """
+  Get information on a dogma attribute.
+
+  ## Response Example
+
+  Information about a dogma attribute:
+
+      %{
+        "attribute_id" => 20,
+        "default_value" => 1,
+        "description" => "Factor by which topspeed increases.",
+        "display_name" => "Maximum Velocity Bonus",
+        "high_is_good" => true,
+        "icon_id" => 1389,
+        "name" => "speedFactor",
+        "published" => true,
+        "unit_id" => 124
+      }
+
+  ## Swagger Source
+
+  This function was generated from the following Swagger operation:
+
+  - `operationId` -- `get_dogma_attributes_attribute_id`
+  - `path` -- `/dogma/attributes/{attribute_id}/`
+
+  [View on ESI Site](https://esi.tech.ccp.is/latest/#!/Dogma/get_dogma_attributes_attribute_id)
+
+  """
+  @spec attribute(attribute_id :: integer) :: ESI.Request.t
+  def attribute(attribute_id) do
+    %ESI.Request{
+      verb: :get,
+      path: "/dogma/attributes/#{attribute_id}/",
+      opts_schema: %{datasource: {:query, :optional}},
+
+    }
+  end
+
+  @doc """
   Returns info about a dynamic item resulting from mutation with a mutaplasmid..
 
   ## Response Example
@@ -137,6 +108,35 @@ defmodule ESI.API.Dogma do
     %ESI.Request{
       verb: :get,
       path: "/dogma/dynamic/items/#{type_id}/#{item_id}/",
+      opts_schema: %{datasource: {:query, :optional}},
+
+    }
+  end
+
+  @doc """
+  Get a list of dogma attribute ids.
+
+  ## Response Example
+
+  A list of dogma attribute ids:
+
+      [1, 2, 3]
+
+  ## Swagger Source
+
+  This function was generated from the following Swagger operation:
+
+  - `operationId` -- `get_dogma_attributes`
+  - `path` -- `/dogma/attributes/`
+
+  [View on ESI Site](https://esi.tech.ccp.is/latest/#!/Dogma/get_dogma_attributes)
+
+  """
+  @spec attributes() :: ESI.Request.t
+  def attributes() do
+    %ESI.Request{
+      verb: :get,
+      path: "/dogma/attributes/",
       opts_schema: %{datasource: {:query, :optional}},
 
     }
